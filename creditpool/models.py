0df46147150976c1c37c07576d46b46d035ccab9
Tom Samstag <github@modtwo.com>
2012-06-20 10:11:57 -0700

    First stab at the models from creditpool's sql

--- creditpool/models.py
+++ creditpool/models.py
@@ -1,3 +1,20 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-# Create your models here.
+class UserProfile(models.Model):
+	user = models.OneToOneField(User)
+	credit = models.DecimalField(max_digits=10, decimal_places=2)
+
+class UserTransfer(models.Model):
+	user = models.ForeignKey(User)
+	credit = models.DecimalField(max_digits=10, decimal_places=2)
+	entered = models.DateTimeField(auto_now_add=True)
+	confirmed = models.BooleanField()
+
+class GlobalTransfer(models.Model):
+	creator = models.ForeignKey(User)
+	date = models.DateField(null=False)
+	entered = models.DateTimeField(auto_now_add=True)
+	description = models.TextField()
+
+
