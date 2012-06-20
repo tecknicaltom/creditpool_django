906670678e05f7410bd93a90018d8d7e188d2d7b
Tom Samstag <github@modtwo.com>
2012-06-20 10:20:08 -0700

    setting up first view and template

--- creditpool/views.py
+++ creditpool/views.py
@@ -1,1 +1,7 @@
-# Create your views here.
+from django.shortcuts import render_to_response
+from django.template import RequestContext
+
+def index(request):
+	context = {
+	}
+	return render_to_response('index.html', RequestContext(request, context))
