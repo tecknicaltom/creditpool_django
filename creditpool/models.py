5c91a370e60f02030fc67820e2fd7da1fe444e89
Tom Samstag <github@modtwo.com>
2012-06-22 01:01:14 -0700

    fixing the models wrt transactions

--- creditpool/models.py
+++ creditpool/models.py
@@ -5,16 +5,15 @@ class UserProfile(models.Model):
 	user = models.OneToOneField(User)
 	credit = models.DecimalField(max_digits=10, decimal_places=2, default=0)
 
-class UserTransfer(models.Model):
-	user = models.ForeignKey(User, null=False)
-	credit = models.DecimalField(max_digits=10, decimal_places=2)
-	entered = models.DateTimeField(auto_now_add=True)
-	confirmed = models.BooleanField()
-
 class GlobalTransfer(models.Model):
-	creator = models.ForeignKey(User)
-	date = models.DateField(null=False)
+	creator = models.ForeignKey(User, null=False)
 	entered = models.DateTimeField(auto_now_add=True)
 	description = models.TextField()
+
+class UserTransfer(models.Model):
+	transfer = models.ForeignKey(GlobalTransfer, null=False)
+	user = models.ForeignKey(User, null=False)
+	credit = models.DecimalField(max_digits=10, decimal_places=2)
+	confirmed = models.BooleanField(default=False)
 
 
