2b04d5ea9c0617316d84c708b5da71da3df410e6
Tom Samstag <github@modtwo.com>
2012-06-22 01:01:46 -0700

    first pass at commiting transactions!

--- creditpool/urls.py
+++ creditpool/urls.py
@@ -4,4 +4,5 @@ urlpatterns = patterns('creditpool.views',
 	url(r'^$', 'index'),
 	url(r'^new_transaction', 'new_transaction'),
 	url(r'^confirm_transaction', 'confirm_transaction'),
+	url(r'^commit_transaction', 'commit_transaction'),
 )
--- creditpool/views.py
+++ creditpool/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render_to_response
+from django.shortcuts import redirect, render_to_response
 from django.template import RequestContext
 from decimal import Decimal
 import re
@@ -10,6 +10,21 @@ def get_users():
 	# and within creditpool)
 	#return [profile.user for profile in UserProfile.objects.all()]
 	return User.objects.filter(userprofile__isnull=False)
+
+def parse_request_amts(params, me):
+	user_pks = []
+	for (key, value) in params.items():
+		result = re.match(r'^val_(?P<pk>\d+)$', key)
+		if result:
+			pk = int(result.group('pk'))
+			if pk != me.pk:
+				user_pks.append(pk)
+	transaction_users = get_users().filter(pk__in=user_pks)
+	my_transaction_amt = Decimal(0)
+	for transaction_user in transaction_users:
+		transaction_user.transaction_amt = Decimal(params['val_%d' % transaction_user.pk])
+		my_transaction_amt -= transaction_user.transaction_amt
+	return (transaction_users, my_transaction_amt)
 
 def index(request):
 	users = get_users()
@@ -35,17 +50,7 @@ def new_transaction(request):
 	return render_to_response('new_transaction.html', RequestContext(request, context))
 
 def confirm_transaction(request):
-	user_pks = []
-	for (key, value) in request.GET.items():
-		result = re.match(r'^val_(?P<pk>\d+)$', key)
-		if result:
-			user_pks.append(result.group('pk'))
-	transaction_users = get_users().filter(pk__in=user_pks)
-	my_transaction_amt = Decimal(0)
-	for transaction_user in transaction_users:
-		transaction_user.transaction_amt = Decimal(request.GET['val_%d' % transaction_user.pk])
-		my_transaction_amt -= transaction_user.transaction_amt
-
+	(transaction_users, my_transaction_amt) = parse_request_amts(request.GET, request.user)
 	old_balance = request.user.userprofile.credit
 	new_balance = old_balance + my_transaction_amt
 
@@ -57,3 +62,19 @@ def confirm_transaction(request):
 			'new_balance': new_balance,
 	}
 	return render_to_response('confirm_transaction.html', RequestContext(request, context))
+
+def commit_transaction(request):
+	(transaction_users, my_transaction_amt) = parse_request_amts(request.POST, request.user)
+
+	transfer = GlobalTransfer(creator=request.user, description=request.POST['descrip'])
+	transfer.save()
+	for transaction_user in transaction_users:
+		user_transfer = UserTransfer(transfer=transfer, user=transaction_user, credit=transaction_user.transaction_amt)
+		user_transfer.save();
+		transaction_user.userprofile.credit += transaction_user.transaction_amt
+		transaction_user.userprofile.save()
+
+	request.user.userprofile.credit += my_transaction_amt
+	request.user.userprofile.save()
+
+	return redirect(index)
